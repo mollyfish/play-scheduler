0d917859d6a76f3923800ecd33c1f7a309eb8648
Molly Kent (InConsulting Inc.) <v-mokent@microsoft.com>
2016-03-04 16:11:26 -0800

    change waiting count

--- schedlua/testy/test_original.lua
+++ schedlua/testy/test_original.lua
@@ -51,7 +51,7 @@ end
 local function main()
 	local t1 = spawn(counter, 1, "counter", 50)
 	local t2 = spawn(counter, 0, "counter", 20)
-	local t3 = spawn(waitingOnCount, 1, "counter", 20)
+	local t3 = spawn(waitingOnCount, 1, "counter", 25)
 	local t4 = spawn(wait15, 1)
 
 --	counter15
