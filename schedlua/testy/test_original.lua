3fd5b26017d947869bdd98f6bb21449a8946b7ec
Molly Kent (InConsulting Inc.) <v-mokent@microsoft.com>
2016-03-04 16:03:07 -0800

    give 20 counter high priority

--- schedlua/testy/test_original.lua
+++ schedlua/testy/test_original.lua
@@ -50,7 +50,7 @@ end
 
 local function main()
 	local t1 = spawn(counter, 1, "counter", 50)
-	local t2 = spawn(counter, 1, "counter", 20)
+	local t2 = spawn(counter, 0, "counter", 20)
 	local t3 = spawn(waitingOnCount, 1, "counter", 20)
 	local t4 = spawn(wait15, 1)
 
